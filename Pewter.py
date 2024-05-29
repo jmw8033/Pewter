ab77d286d455a827bb0cc0db13ae2407e71caf9b
Joey Westhoff <jmw8033@psu.edu>
2024-05-29 11:04:36 -0400

    added delay between starting threads to avoid race condition with logo

--- Pewter.py
+++ Pewter.py
@@ -1,11 +1,13 @@
 from EmailProcessor import EmailProcessor
 import threading
 import config
+import time
     
 if __name__ == "__main__":
     # ACP
     acp_email_processor = threading.Thread(target=EmailProcessor, args=(config.ACP_USER, config.ACP_PASS))
     acp_email_processor.start()
+    time.sleep(0.1)
 
     # APC
     apc_email_processor = threading.Thread(target=EmailProcessor, args=(config.APC_USER, config.APC_PASS))
