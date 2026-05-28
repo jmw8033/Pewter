7adb73bd4bbda5be5c7af3915e225029ccb8cae1
Joey Westhoff <jmw8033@psu.edu>
2026-05-28 11:02:37 -0400

    changed duplicate invoice renaming to be random
    removed extra import

--- EmailProcessor.py
+++ EmailProcessor.py
@@ -4,7 +4,6 @@ from tkinter import ttk, messagebox
 from datetime import datetime
 from math import ceil
 import threading
-import importlib
 import traceback
 import win32print
 import win32gui
@@ -12,6 +11,8 @@ import win32api
 import socket
 import json
 import ssl
+import random
+import string
 import imaplib
 import smtplib
 import sqlite3
@@ -593,7 +594,7 @@ class EmailProcessor:
 
                 # Check if file already exists
                 if os.path.exists(filepath):
-                    filename = f"{filename[:-4]}_{str(int(time.time()))}.pdf"
+                    filename = f"{filename[:-4]}_{''.join(random.choices(string.ascii_letters + string.digits, k=10))}.pdf"
                     filepath = os.path.join(self.INVOICE_FOLDER, filename)
                     self.log(f"New invoice file already exists, renamed {filename}", tag="orange")
 
@@ -611,7 +612,7 @@ class EmailProcessor:
                     new_filepath = template_exists[0]
                     # Check if file already exists
                     if os.path.exists(new_filepath):
-                        filename = f"{os.path.basename(new_filepath)[:-4]}_{str(int(time.time()))}.pdf"
+                        filename = f"{os.path.basename(new_filepath)[:-4]}_{''.join(random.choices(string.ascii_letters + string.digits, k=10))}.pdf"
                         new_filepath = os.path.join(self.INVOICE_FOLDER, filename)                        
                         self.log(f"New invoice file already exists, renamed {filename}", tag="orange")
                         
@@ -748,7 +749,7 @@ class EmailProcessor:
             if os.path.exists(new_filepath):
                 old_filepath = new_filepath
                 self.log(f"New invoice file already exists at {os.path.basename(old_filepath)} -- {self.current_time} {self.current_date}", tag="orange")
-                new_filepath = f"{old_filepath[:-4]}_{str(int(time.time()))}.pdf"
+                new_filepath = f"{old_filepath[:-4]}_{''.join(random.choices(string.ascii_letters + string.digits, k=10))}.pdf"
 
             # Save invoice
             os.rename(filepath, new_filepath)
